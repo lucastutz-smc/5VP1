d781a7510c7952d5433283ce9fe4341098dfd7f0
lucastutz-smc <144335972+lucastutz-smc@users.noreply.github.com>
2023-10-06 12:52:15 +0000

    Appel en bommen gemaakt

---
+++ Overloper.js
@@ -0,0 +1,203 @@
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = r;
+    this.aantalKolommen = k;
+    this.celGrootte = null;
+  }
+  
+  berekenCelGrootte() {
+    this.celGrootte = canvas.width / this.aantalKolommen;
+  }
+  
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
+class Jos {
+  constructor() {
+    this.x = 400;
+    this.y = 300;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+  }
+  
+  beweeg() {
+    if (keyIsDown(65)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(68)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(87)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(83)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+    
+    this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+    
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+  
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}  
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+  
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+class Bom {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+  
+  beweeg() {
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+const rijen = 12;
+const kolommen = 18;
+
+let bommen = [];
+var aantalLevens = 3; 
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(900,600);
+  canvas.parent();
+  frameRate(10);
+  textFont("Verdana");
+  textSize(90);
+  
+  raster = new Raster(rijen,kolommen);
+  
+  raster.berekenCelGrootte();
+  
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
+
+  bob = new Vijand(600,400);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
+
+  for (let i = 0; i < 5; i++) {
+    bommen.push(new Bom(floor(random(0,kolommen/2))*raster.celGrootte+kolommen/2*raster.celGrootte,floor(random(0,rijen))*raster.celGrootte));
+    bommen[i].stapGrootte = floor(random(1,4))*eve.stapGrootte;
+    bommen[i].sprite = loadImage("images/sprites/bom_100px.png")
+  } 
+  
+}
+
+function draw() {
+  background(brug);
+  raster.teken();
+  eve.beweeg();
+  alice.beweeg();
+  bob.beweeg();
+  eve.toon();
+  alice.toon();
+  bob.toon();
+
+
+bommen[0].beweeg();
+bommen[0].toon();
+  
+  for (let i = 0; i < bommen.length; i++) {
+    bommen[i].beweeg();
+    bommen[i].toon();
+  } 
+  
+  push();
+  fill('black');
+  textSize(30);
+  text(aantalLevens,30,300);
+  pop();
+  
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+    aantalLevens-=1
+  }
+  
+  
+  if (aantalLevens < 1){
+    background('black');
+    fill('red');
+    text("Je bent dood :(",50,300);
+    noLoop();
+  }
+  
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("Je hebt gewonnen!",30,300);
+    noLoop();
+  }
+}
