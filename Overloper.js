3c1db5a7e8da45a9607fd181504b8b35bf694129
Lucas da Mata Stutz <144335972+lucastutz-smc@users.noreply.github.com>
2023-10-23 12:34:17 +0200

    oranje kolom en rij

--- Overloper.js
+++ Overloper.js
@@ -15,6 +15,9 @@ class Raster {
     stroke('grey');
     for (var rij = 0;rij < this.aantalRijen;rij++) {
       for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        if (rij = 0 || kolom = 0) {
+          stroke('orange')
+        }
         rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
       }
     }
